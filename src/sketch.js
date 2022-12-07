aca16ed8620121823f1d11300bdd8a6fa434aea3
dev6a73 <dev6a73@gmail.com>
2022-12-07 15:55:09 +0900

    firework

--- src/sketch.js
+++ src/sketch.js
@@ -1,6 +1,7 @@
 let stars
+let canvas
 function setup() {
-  const canvas = createCanvas(windowWidth, windowHeight);
+  canvas = createCanvas(windowWidth, windowHeight);
   noStroke();
   fill(0, 0, 0)
   stars = Array(windowWidth*windowHeight/2500|0).fill(0).map((v, i)=>[Math.random()*windowWidth, Math.random()*windowHeight, Math.random()*255, Math.random()*8]);
@@ -10,10 +11,13 @@ let positions = [];
 
 function draw() {
   background(0, 20);
+  const black = color(191*time_, 255*time_, 255*time_)
+  const blue = color(0+127*time_, 32+191*time_, 63+255*time_)
   for (let i = 0; i < windowHeight; i++) {
-    stroke(0, i/windowHeight*31, i/windowHeight*63)
+    stroke(lerpColor(black, blue, i/windowHeight))
     line(0, i, windowWidth, i)
   }
+  if (time_<.5)
   stars.forEach(v=>{
     stroke(v[2]/2+128, 191+v[2]/4, 256-v[2]/2)
     strokeWeight(v[3])
@@ -30,6 +34,7 @@ function draw() {
 }
 
 function mouseClicked() {
+  if (time_) return
   resetColor()
   for (let i = 0; i < 100; i++) {
     const a = Math.random()*windowWidth
---
+++ src/time.js
@@ -0,0 +1,18 @@
+let time_ = 1
+const button = document.getElementById('time')
+
+button.onclick = async e => {
+    button.onclick = undefined
+    button.style.display = 'none'
+    await f()
+}
+
+const sleep = ms => new Promise(r => setTimeout(r, ms));
+
+async function f() {
+    const start = Date.now()
+    while (time_ > 0) {
+        time_ -= start
+        await sleep()
+    }
+}
